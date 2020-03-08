f0cb06ad97b20f4d8fb6dd11a5222495aa256c57
mivehk <k.mivehnejad@gmail.com>
2020-03-08 19:15:42 -0400

    mkdir controllers/route/model - created four endpoints for vsanRoutes to use the app

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 //using babel allows latest ES6 syntax instead of old "require" method of adding classes
 
 import express from 'express';
+import routes from './src/routes/vsanRoutes'
 
 const app = express();
 const PORT = 6969;
 
+routes(app)
 
 app.get('/' , (req,res) =>
     res.send(`This is a VSAN API on port ${PORT}`)
---
+++ src/routes/vsanRoutes.js
@@ -0,0 +1,26 @@
+const routes = (app) => {
+
+    app.route('/cluster')
+
+    .get((req,res) =>
+     res.send('GET request successful!')
+    )
+
+    .post((req,res) =>
+    res.send('POST request successful!')
+    
+    )
+ 
+    app.route('/cluster/:clusterID')
+
+    .put((req,res) =>
+    res.send('PUT request successful!')
+    )
+
+    .delete((req,res) =>
+    res.send('DELETE request successful!')
+    )
+
+}
+
+export default routes;
